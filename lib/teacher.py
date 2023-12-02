46da8469791c4130fc7358cbcc6283b9a5068dbd
joshua chivile <joshuachivile@gmail.com>
2023-12-02 03:33:42 +0300

    Class Inheritance with Python

--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python
 
-from user import User
+from user import User # importing user class
 
 import random
 
+knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
+
 class Teacher(User):
 
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
+    
     def teach(self):
-        pass
+        index = random.randint(0,len(self.knowledge))
+        return self.knowledge[index]
+
+teacher = Teacher("Joshua","Chivile")
+teacher.teach()
